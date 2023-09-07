e9c4bc894367fd5f32bce9ff97b085450da00377
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-07 15:34:30 -0700

    the more simple the more fun

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
