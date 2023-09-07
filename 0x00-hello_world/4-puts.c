38019376dda2183546917024f55a2cc7d1c084f0
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-07 12:38:48 -0700

    the first program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- * including the IO LIBRARY
  * main - Entry point
  *
  * description 'this program is to print a simple word'
- *@parameter: describe the parameter
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
