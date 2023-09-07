cff7ab2a14f6fb75acfcca6e90a55cc5ec87d9e6
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-07 11:59:13 -0700

    the first program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	/* my first C program
-	 * this program is to print a simple word
-	 */
+	/* my first C progrm, this program is to print a simple word*/
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
