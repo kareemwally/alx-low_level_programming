61577359046d9811a818b17877198720853e232d
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-07 11:52:53 -0700

    the first program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-int main()
+int main(void)
 {
+	/*
+	 * this program is to print a simple word
+	 * */
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
