4ddaf803ce02818b4794072056b6eda546634216
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-09 20:13:49 -0700

    The ASCII table is important as F

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the idea is to convert the ascii code in the putchar function'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 102 ; i++)
+{
+if (i == 57)
+i = 97;
+i = putchar(i);
+}
+putchar('\n');
+return (0);
+}
