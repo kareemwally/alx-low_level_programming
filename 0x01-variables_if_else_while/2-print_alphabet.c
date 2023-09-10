3d587e7f43da76457c54d66eb6ddf2258eede792
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-09 18:21:21 -0700

    for loop and putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'This program to print the alphabet by for loop'
+ *
+ *Return: Always 0 (Success)int main(void)
+ */
+
+int main(void)
+{
+char i;
+
+for (i = 'a' ; i <= 'z' ; i++)
+{
+
+putchar(i);
+}
+	return (0);
+}
+
