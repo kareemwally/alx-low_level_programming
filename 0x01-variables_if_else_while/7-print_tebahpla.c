379594fe2a8de1731cb200a6799544a38e12de69
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-09 19:32:33 -0700

    reversed lowercase

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'this program is to print lowercase reversed alphabet'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char i;
+
+for (i = 'z'; i > 'a'; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
