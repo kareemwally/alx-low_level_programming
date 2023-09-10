d85f4fcf7e9075de39fb69cc1210aa615714a842
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-09 17:00:43 -0700

    greater or less than zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,10 +16,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf("%d is positive\n",n);
+printf("%d is positive\n", n);
 else if (n == 0)
 printf("0 is zero\n");
 else
-printf("%d is negative",n);
+printf("%d is negative\n", n);
 return (0);
 }
