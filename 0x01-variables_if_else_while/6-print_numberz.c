2ef336f06ccf90b2fde15087c85b42012b398228
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-09 19:07:31 -0700

    no char type

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+short i = '0';
+
+while (i < '10')
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
