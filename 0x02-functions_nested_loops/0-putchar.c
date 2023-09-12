af60e2d10b87948ee3a867bc04b76d2099b616d5
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-11 21:17:18 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * 
+ * Description: 'This simple program is just to print one sentence'
+ * 
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+printf("_putchar");
+    return 0;
+}
