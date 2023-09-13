84ed312b6873d77e4bf4595a7d76e6a9c32134bc
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-13 08:25:28 -0700

    summing up

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,8 +1,19 @@
-#include"main.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ * Description:'This program is to sum the 3 or 5-factored numbers'
+ * Return:int always 0 (success)
+ */
+int main(void)
+{
+comp_add();
+return (0);
+}
 /**
  * comp_add - function
- * Description:'This program is to sum the 3 or 5-factored numbers'
- * Return:void
+ * Description:3,5 factored numbers 0:1024 summation
+ * Returns:void
  */
 void comp_add(void)
 {
