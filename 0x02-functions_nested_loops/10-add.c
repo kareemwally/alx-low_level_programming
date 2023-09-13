4ff480c22cbc88a9819c409eaf05e228bf991639
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-13 06:28:21 -0700

    The adding

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * add - function
+ *
+ * Description:'This simple program is just to return summation a+b'
+ *@a:the first number to sum
+ *@b:the second number to sum
+ * Return:int (summation)
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
