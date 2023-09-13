8c02bb43483489bee489879fba01241b27125797
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-13 07:54:20 -0700

    N mul table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,19 +8,16 @@
 void print_times_table(int n)
 {
 int i, j;
-if (n > 15 || n < 0)
+if (!(n > 15 || n < 0))
 {
-putchar(0);
-n = -1 * n;
-}
-for (i = 0; i <= n; i++)
+for (i = 0; i <= n;i++)
 {
 for (j = 0; j <= n; j++)
 {
-printf("%d", (i * j));
+printf("%d",(i*j));
 if (j != n)
 {
-if (i * (j + 1) > 99)
+if (i * ( j + 1) > 99)
 {
 printf(", ");
 }
@@ -35,5 +32,6 @@ printf(",   ");
 }
 }
 putchar('\n');
+}
 }
 }
