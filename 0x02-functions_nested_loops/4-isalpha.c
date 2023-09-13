af069db88241d0e16ce3e53955e25b0444ba4c66
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-12 18:31:53 -0700

    only alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * _isalpha - function
+ *
+ * Description:'This program is to checks alphabet letters'
+ * @c:the character we want to check
+ *
+ * Return:1 if alpha(Success) or 0 anything else
+ */
+_isalpha(int c)
+{
+int i;
+if ((c >= 97 && c < 123) || (c > 64 && c < 90))
+i = 1;
+else
+i = 0;
+return (i);
+}
