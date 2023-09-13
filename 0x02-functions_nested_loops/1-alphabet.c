94e4aebe6204900a82c1e1c81a6bcf1ed83a6dfa
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-12 17:27:03 -0700

    printing the alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,5 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Description:'IN this function other fuctions will be invoked '
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-print_alphabet();
-return (0);
-}
-/**
  * print_alphabet - Functoion
  *
  * Description:'the body of the previous function'
