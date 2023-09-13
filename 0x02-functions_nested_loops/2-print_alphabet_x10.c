6a860a8de757b050a259326767f78dfc826c74d6
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-12 17:40:54 -0700

    printing alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,14 @@
 #include"main.h"
 /**
  * print_alphabet_x10 - function
- * Description:'This program is to print a the alphabet using prototyped function'
+ *
+ * Description:'This program is to print alphabet 10 times'
  *
  * Return:void it just prints (Success)
  */
 void print_alphabet_x10(void)
 {
-int i, j=0;
+int i, j = 0;
 while (j != 10)
 {
 for (i = 97; i <= 122; i++)
