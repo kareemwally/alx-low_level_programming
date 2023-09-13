a3f2fb1a7ef726c5663d6c91acd1c183a15c5756
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-13 06:20:27 -0700

    0:9 mul table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,24 +8,22 @@
  */
 void times_table(void)
 {
-int i, j, product;
+int i, j;
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
 {
-product = i * j;
-printf("%d",product);
+printf("%d", (i * j));
 if (j != 9)
 {
-printf(",");
-if (product <= 9)
+if (i * (j + 1) > 9)
 {
-printf("  ");
+printf(", ");
 }
 else
-printf(" ");
+printf(",  ");
+}
 }
 }
 putchar('\n');
-}
 }
