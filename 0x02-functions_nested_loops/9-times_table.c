319ab81f30e3fefae88f0e1bff964cca422b3e74
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-13 05:14:37 -0700

    0:9 mul table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ * times_table - function
+ *
+ * Description:'This program is to print the full multiplication table 0:9'
+ *
+ * Return:Void it just prints the table
+ */
+void times_table(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+printf("%d", (i * j));
+if (j != 9)
+printf(",\t");
+}
+putchar('\n');
+}
+}
