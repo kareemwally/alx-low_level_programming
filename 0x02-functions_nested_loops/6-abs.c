7d34e8dbbdb5b2749d648fcbe5241a8cfff770d4
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-12 19:32:30 -0700

    the modulus

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * _abs - function
+ *
+ * Description:'This program is to return the mod of n'
+ * @n:the number we want to check
+ * Return:Modulus of n(always poitive) or 0
+ */
+int _abs(int n)
+{
+int i = n;
+if (n < 0)
+i = -1 * n;
+return i;
+}
