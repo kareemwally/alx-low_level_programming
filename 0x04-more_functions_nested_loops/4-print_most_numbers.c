8f50da48beed31a1ed52ac8b0954a4837621069c
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-14 11:18:32 -0700

    not 2,4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * print_most_numbers - Function
+ * Description:'This program is to print digits but 2 ,4'
+ * Return:void (prints digits)
+ */
+void print_most_numbers(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+if (i == 50 || i == 52)
+{
+continue;
+}
+_putchar(i);
+}
+}
