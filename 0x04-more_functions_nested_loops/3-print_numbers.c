1b5b7e8c9513ccc67abef5761e757214584a210e
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-14 05:38:39 -0700

    printing numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * print_numbers - Function
+ * Description:'This program is to print digits'
+ * Return:void (prints digits)
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
