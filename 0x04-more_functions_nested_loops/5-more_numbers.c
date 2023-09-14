65fe17da87757fa1fef1dce434cd68e8cd44be59
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-14 11:19:37 -0700

    printing 0:14 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * more_numbers - Function
+ * Description:'This program is to print numbers 0:14'
+ * Return:void (prints digits)
+ */
+ void more_numbers(void)
+{
+int i, j = 10;
+while (j > 0)
+{
+for (i = 0; i < 15; i++)
+{
+printf("%d",i);
+}
+printf("\n");
+j--;
+}
+}
