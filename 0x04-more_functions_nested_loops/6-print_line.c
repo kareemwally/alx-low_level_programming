3f00fd09eb17753ed88c7dc25f4abd24032e4a68
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-14 11:44:59 -0700

    straight line with(_)

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * print_line - Function
+ * Description:'This program is to print line n times'
+ * @n: te number of(_)
+ * Return:void (prints digits)
+ */
+void print_line(int n)
+{
+while (n > 0)
+{
+_putchar('_');
+n--;
+}
+_putchar('\n');
+}
