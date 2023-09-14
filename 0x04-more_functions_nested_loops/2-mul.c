88364701b48ceb5d25edb166afbd82b8f22834df
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-14 00:04:43 -0700

    the multuplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * mul - Function
+ * Description:'This program is to check only number characters'
+ *@a:the first num
+ *@b:the second num
+ * Return:int (multiplication)
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
