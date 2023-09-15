66f5bf2c70a9506f6db01038464a232d712c343d
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-15 00:08:23 -0700

    the heighest prime number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include"main.h"
+/**
+ * main - Entry point
+ * Description:'This program is to print the biggest prime num'
+ * Return:void (prints digits)
+ */
+int main(void)
+{
+int i, j, res = 2, N;
+for (i = 2; i <= N; i++)
+{
+for (j = 2; j <= i; j++)
+{
+if (i % j == 0 && j != i)
+{
+break;
+}
+else if (i % j == 0 && j == i)
+{
+if (i > res)
+res = i;
+}
+}
+}
+printf("%d\n", res);
+return (0);
+}
