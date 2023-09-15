a60bf89306f2a24ceba12a480172b2272753a898
kareemwally <kareemwally@std.mans.edu.eg>
2023-09-14 19:14:26 -0700

    the fizzbuzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include"main.h"
+/**
+ * main - Entry point
+ * Description:'This program is to check %3 and %5'
+ * Return:void (prints digits)
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100;  i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+continue;
+}
+else if (i % 5 == 0)
+printf("Buzz ");
+else if (i % 3 == 0)
+printf("Fizz ");
+else
+printf("%d ", i);
+}
+return (0);
+}
