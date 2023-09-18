6cd8d90ad26c1333c8fbf4a66bff5ac1329814a0
kareemwally <123824460+kareemwally@users.noreply.github.com>
2023-09-18 20:29:16 +0300

    only to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * reset_to_98-function
  * Description:this function is to change variable's value
- *@*n: the address of the variable
+ *@n: the address of the variable
  *return:void
  */
 void reset_to_98(int *n)
