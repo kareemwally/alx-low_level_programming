c8c2a5fe086eb38339570cbc438f6c0fe92300a4
kareemwally <123824460+kareemwally@users.noreply.github.com>
2023-09-18 23:00:01 +0300

    reversely too

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ *rev_string - function
+ *description: this function prints string reversely
+ *@s:the string we wanna print reversely
+ *Return:void
+ */
+void rev_string(char *s)
+{
+int i, a = strlen(s);
+for (i = 0; i < a; i++)
+{
+printf("%c", s[i]);
+}
+}
