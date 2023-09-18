9f7f8001635d2389bf5e4e1dd4e1aea775688a69
kareemwally <123824460+kareemwally@users.noreply.github.com>
2023-09-18 21:56:17 +0300

    How long?

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,8 @@
-#include"main.h"
 /**
  *_strlen - function
  *description: this function returns the length of string
- *return:int (length of string)
+ *@s:the string we wanna check
+ *return: int (length of string)
  */
 int _strlen(char *s)
 {
