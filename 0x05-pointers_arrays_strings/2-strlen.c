b0b4d9ad109bd85224fb02b1ccf78e2a5ab54737
kareemwally <123824460+kareemwally@users.noreply.github.com>
2023-09-18 22:02:07 +0300

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,7 @@
  *_strlen - function
  *description: this function returns the length of string
  *@s:the string we wanna check
- *return: int (length of string)
+ *return: integer greater than or equal zero (length of string)
  */
 int _strlen(char *s)
 {
