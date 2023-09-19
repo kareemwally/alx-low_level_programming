3c44c3522b3defd3c626287bac8611d5b3e2a635
kareemwally <123824460+kareemwally@users.noreply.github.com>
2023-09-19 20:10:18 +0300

    n of all

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ *print_array -Function
+ *Description:this function is to print n numbers of array
+ *@a:the int array
+ *@n:the number of elements to print
+ *Return:void
+ */
+void print_array(int *a, int n)
+{
+for (i = 0; i < n - 1; i++)
+{
+printf("%d, ", a[i]);
+}
+printf("\n");
+}
