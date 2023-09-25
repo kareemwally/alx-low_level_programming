1abd08e4f7f23cfd61203fd4e0a4b8dfe84ca529
kareemwally <123824460+kareemwally@users.noreply.github.com>
2023-09-25 15:55:17 +0300

    Create main.h

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef main_h
+#define main_h
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+#endif
