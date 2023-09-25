a802aaa0795b3cc89819d4bb5b274c322600be87
kareemwally <123824460+kareemwally@users.noreply.github.com>
2023-09-26 02:24:49 +0300

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,6 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
- *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+void print_diagsums(int *a, int size);
+void print_chessboard(char (*a)[8]);
+char *_strstr(char *haystack, char *needle);
+char *_strpbrk(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
 #endif
