9595970da5b207775450d9b22d6007a7f050d8bd
kareemwally <123824460+kareemwally@users.noreply.github.com>
2023-09-26 03:17:33 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,1 +1,13 @@
-
+#include"main.h"
+/**
+ *_strspn -Function
+ *Description:finds the length of suitable substring of (s)
+ *@s:the string we wanna check its substring
+ *@accept:the string we wanna match
+ *return:(int)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int res = strspn (s, accept);
+return (res);
+}
