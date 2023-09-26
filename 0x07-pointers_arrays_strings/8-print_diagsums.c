c12538cefee60929e4a9880a3b5f668f40f1580a
kareemwally <123824460+kareemwally@users.noreply.github.com>
2023-09-26 04:01:17 +0300

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,1 +1,21 @@
-
+#include"main.h"
+/**
+ *print_diagrams -Function
+ *Description:prints the sum of diagonal
+ *@a:the 2-d array
+ *@size:the square matrix size
+ *return:(void)
+ */
+void print_diagsums(int *a, int size)
+{
+int i, j ,res = 0;
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+if (i == j)
+res += a[i][j];
+}
+}
+printf("%d", res);
+}
