2cadd46c76f01607be7bdaa5879a2a94f8e0e6c9
kareemwally <123824460+kareemwally@users.noreply.github.com>
2023-09-26 03:50:25 +0300

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,1 +1,19 @@
-
+#include"main.h"
+/**
+ *print_chessboard -Function
+ *Description:prints the chessboard
+ *@a:the board representation
+ *return:(void)
+ */
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+printf("%c", a[i][j]);
+}
+printf("\n");
+}
+}
