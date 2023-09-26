8f848bbbf69f0b8aed5d91e3680cdcb20671bf7f
kareemwally <123824460+kareemwally@users.noreply.github.com>
2023-09-26 03:41:51 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,1 +1,13 @@
-
+#include"main.h"
+/**
+ *_strstr -Function
+ *Description:matches 1st occurrence of needle in the haystack
+ *@haystack:the string we wanna check
+ *@needle:the substring we wanna find in haystack
+ *return:(pointer)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+char *res = strstr(haystack, needle);
+return (res);
+}
