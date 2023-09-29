9ada3c4468e7b5f8dd686f08baa01f17aeba9912
kareemwally <123824460+kareemwally@users.noreply.github.com>
2023-09-29 04:40:19 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,1 +1,21 @@
 #include"main.h"
+/**
+ *is_palindrome-Function
+ *Description:checks whether s is palindrome or not
+ *@s:the string we're checking
+ *Return :(int) 1,0
+ */
+ int is_palindrome(char *s)
+{
+int res = 1, size = strlen(s);
+if (s[i] == s[size - i - 1])
+{
+i++;
+is_palindrome(s);
+}
+else
+{
+res = 0;
+}
+return (res);
+}
