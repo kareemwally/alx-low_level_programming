37310946f108f0a77469cc0771246dc426542e22
kareemwally <123824460+kareemwally@users.noreply.github.com>
2023-09-29 04:55:18 +0300

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,7 +6,6 @@
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int i = 0;
-int res = 1;
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
