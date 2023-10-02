9ae9cc2de5bf56527c50da53ee483dc9376d4769
kareemwally <123824460+kareemwally@users.noreply.github.com>
2023-10-02 03:36:53 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ *check-function
+ *Description:checks if n less or equal 1
+ *@n:the number we wanna check
+ *return: (int) 1- zero
+ */
 int check(int n)
 {
 if (n <= 1)
@@ -9,6 +15,12 @@ else
 i = 1;
 return (i);
 }
+/**
+ *is_prime_number-function
+ *description:checks if n is prime or not
+ *@n:the number we wanna check
+ *return: (int) 1-0 
+ */
 int is_prime_number(int n)
 {
 int res = 0;
