355d53f65a4c3d83731b6f305942d746d425e0e9
kareemwally <123824460+kareemwally@users.noreply.github.com>
2023-10-02 03:38:36 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,7 +3,7 @@
  *check-function
  *Description:checks if n less or equal 1
  *@n:the number we wanna check
- *return : (int) 1- zero
+ *Return: (int) 1- zero
  */
 int check(int n)
 {
@@ -19,7 +19,7 @@ return (i);
  *is_prime_number-function
  *description:checks if n is prime or not
  *@n:the number we wanna check
- *return : (int) 1-0
+ *Return: (int) 1-0
  */
 int is_prime_number(int n)
 {
