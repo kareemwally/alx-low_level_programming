357f2ea9032263976d8c418405c8ffecf4bb1c06
kareemwally <123824460+kareemwally@users.noreply.github.com>
2023-10-05 02:21:42 +0300

    Update main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -9,5 +9,6 @@ void free_grid(int **grid, int height);
 int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
+int i;
 char *create_array(unsigned int size, char c);
 #endif
