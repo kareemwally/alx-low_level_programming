df3e93185b373e8e38fda529b9ea056ae290c6cd
kareemwally <123824460+kareemwally@users.noreply.github.com>
2023-10-05 01:55:04 +0300

    Create main.h

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef main_h
+#define main_h
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+#include <string.h>
+void free_grid(int **grid, int height);
+int **alloc_grid(int width, int height);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+char *create_array(unsigned int size, char c);
+#endif
