facd87e700cd31fd24fee6c214304688bafe1d3e
kareemwally <123824460+kareemwally@users.noreply.github.com>
2023-10-05 03:03:14 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,7 +1,7 @@
 #include"main.h"
 char *_strdup(char *str)
 {
-char *s =(char *) strdup(strlen(str));
+char *s =(char *) strdup((long unsigned int)strlen(str));
 if (s == NULL || str == NULL)
 return (NULL);
 else
