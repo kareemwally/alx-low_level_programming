b7d7c6cf3bb44c97d36969e6609d9d5148d3e444
kareemwally <123824460+kareemwally@users.noreply.github.com>
2023-10-05 03:13:33 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,9 +1,19 @@
 #include"main.h"
+/**
+ *_strdup-Function
+ *Description :this function allocates the same size of memory as str
+ *@str:the string we wanna match
+ *Return :(pointer)
+ */
 char *_strdup(char *str)
 {
-char *s =(char *) strdup((const char*)strlen(str));
+char *s =malloc(strlen(str));
 if (s == NULL || str == NULL)
 return (NULL);
 else
+{
+for (i = 0; i < strlen(str); i++)
+s[i] = str[i];
+} 
 return (s);
 }
