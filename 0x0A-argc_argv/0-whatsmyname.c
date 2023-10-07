4ce4cb9308a1b7390b8703e4960b9ba165223dcf
kareemwally <kareemwally@std.mans.edu.eg>
2023-10-06 23:36:23 -0700

    how about now?

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ *main-entry point
+ *description:the main function just to print the file name
+ *@argc:the count of params passed to main during execution
+ *@argv:the parameters
+ Return: (int) 0 success.
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
