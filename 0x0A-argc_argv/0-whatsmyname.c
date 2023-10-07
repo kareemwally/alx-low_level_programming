33900941fd4d8abe2d443a1ed2f8b16567c9a934
kareemwally <kareemwally@std.mans.edu.eg>
2023-10-07 01:48:53 -0700

    finally

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,7 +4,7 @@
  *description:the main function just to print the file name
  *@argc:the count of params passed to main during execution
  *@argv:the parameters
- *Return :(int)
+ *Return: Always 0 (Success)
  */
 int main(int argc, char const *argv[])
 {
