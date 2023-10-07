01fa992a72e06ec1b5f8d1f065fb347211e9b87d
kareemwally <kareemwally@std.mans.edu.eg>
2023-10-06 23:56:41 -0700

    the params are:

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ *main-entry point
+ *description:the main function just to print the file name
+ *@argc:the count of params passed to main during execution
+ *@argv:the parameters
+ *Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
