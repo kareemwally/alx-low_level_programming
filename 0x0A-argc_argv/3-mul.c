6f9cf10dcecc3e1a5645bd89c51ef235e5c80b92
kareemwally <kareemwally@std.mans.edu.eg>
2023-10-07 18:50:43 -0700

    atoi?

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,13 +8,14 @@
  */
 int main(int argc, char *argv[])
 {
-int i;
 if (argc <= 1)
 {
 printf("Error\n");
 return (1);
 }
-int mul = argv[1] * argv[2];
-printf("%d\n", mul);
+int i, j;
+i = atoi(argv[1]);
+j = atoi(argv[2]);
+printf("%d\n", i * j);
 return (0);
 }
