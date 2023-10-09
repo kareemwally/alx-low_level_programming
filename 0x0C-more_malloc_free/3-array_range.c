238a269595f6804677dbcdf6f6d4970071f3c29c
kareemwally <kareemwally@std.mans.edu.eg>
2023-10-08 18:14:37 -0700

    from min to max

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+/**
+ *array_range-function
+ *description:the function craetes pointer like this [min....max]
+ *@min:the least value in the array
+ *@max:the biggest value in the array
+ *Return: (pointer)
+ */
+int *array_range(int min, int max)
+{
+int *res, i, size;
+(void)res;
+if (min > max)
+return (NULL);
+else
+{
+size = max - min + 1;
+res = calloc(size, sizeof(int));
+}
+if (res == NULL)
+return (NULL);
+else
+for (i = 0; i < size; i++)
+{
+res[i] = min;
+min++;
+}
+return (res);
+}
