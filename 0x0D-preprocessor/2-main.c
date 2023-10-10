7d531cd7c8731270a9b99e3b0f94263077a113d8
kareemwally <kareemwally@std.mans.edu.eg>
2023-10-10 10:45:53 -0700

    the name of source file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,11 +3,9 @@
 /**
  *main-entry point
  *description:the main function just to print the file name
- *@argc:the count of params passed to main during execution
- *@argv:the parameters
  *Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main()
 {
 printf("%s\n", __FILE__);
 return (EXIT_SUCCESS);
