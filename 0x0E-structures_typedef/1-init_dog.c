ac7b8bd95141f9fe29eb6730c4f29dd0963a0ee4
kareemwally <kareemwally@std.mans.edu.eg>
2023-10-10 19:36:58 -0700

    intial values

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include"dog.h"
+#include<stddef.h>
 /**
  *int_dog-Function
  *@name:the dog's name
@@ -13,8 +14,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 if (d != NULL)
 {
-d->age = age;
-d->name = name;
-d->owner = owner;
+(*d).age = age;
+(*d).name = name;
+(*d).owner = owner;
 }
 }
