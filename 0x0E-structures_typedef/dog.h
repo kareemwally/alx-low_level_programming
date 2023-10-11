c6f04de0f822dc2cfa4f3355766274d334103221
kareemwally <kareemwally@std.mans.edu.eg>
2023-10-10 18:53:21 -0700

    the poppies

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ *dog -struct
+ *@name: the dog's name
+ *@age: the dog's age
+ *@owner: the owner of dog
+ *
+ *Description: the structure is only for poppies
+ */
 struct dog
 {
 char *name;
