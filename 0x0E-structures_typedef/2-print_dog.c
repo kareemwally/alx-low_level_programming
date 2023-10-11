0c836f48f891f1dbf8c1d2125526fad79378c52b
kareemwally <kareemwally@std.mans.edu.eg>
2023-10-11 05:57:06 -0700

    printing

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,5 +11,16 @@
 void print_dog(struct dog *d)
 {
 if (d != NULL)
-printf("Name: %s\nAge: %f\nOwner: %s", (*d).name, (*d).age, (*d).owner);
+{
+printf("Name: %s\n", (*d).name == NULL ? "(nil)" : (*d).name);
+if ((*d).age != NULL)
+{
+printf("Age: %f\n:", (*d).age);
+}
+else
+{
+printf("Age: (nil)\n:");
+}
+printf("Owner: %s", (*d).owner == NULL ? "(nil)" : (*d).owner);
+}
 }
