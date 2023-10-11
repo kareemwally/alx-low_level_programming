dea9165b17d7f00eb24b2c31db0196aef7282a4b
kareemwally <kareemwally@std.mans.edu.eg>
2023-10-10 19:58:16 -0700

    print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,6 @@
 #include"dog.h"
+#include<stdio.h>
+#include<stddef.h>
 /**
  *print_dog -function
  *description: this function prints fields of struct
@@ -8,8 +10,6 @@
 
 void print_dog(struct dog *d)
 {
-if (d == NULL)
-printf("");
-else
+if (d != NULL)
 printf("Name: %s\nAge: %f\nOwner: %s", (*d).name, (*d).age, (*d).owner);
 }
