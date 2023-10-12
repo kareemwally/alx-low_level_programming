b97fe9273587988884b65c6ce24d51a6799092aa
kareemwally <kareemwally@std.mans.edu.eg>
2023-10-11 18:42:43 -0700

    the array operation

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include"200~function_pointers.h"
+/**
+ *array_iterator-function
+ *@array: array of element to be printed
+ *@size:the size of the array
+ *@action:pointer to a specific function
+ *Return:(void)
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+if (size > 0 && action)
+{
+action(array;
+}
+}
