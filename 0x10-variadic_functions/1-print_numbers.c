ec00cde2b6f34bf076de1697036ed4dfa582330b
kareemwally <kareemwally@std.mans.edu.eg>
2023-10-12 11:22:20 -0700

    printing in pattern

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+/**
+ *print_numbers-function
+ *description:this function prints numbers in a specific pattern
+ *@separator:char pointer to the splitting char
+ *@n:number of elements
+ *Return:(void)
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+va_list res;
+va_start(res, n);
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(res, int));
+if (i != n - 1)
+{
+if (separator != (char *)NULL)
+{
+printf("%c", separator);
+}
+printf(" ");
+}
+}
+va_end(res);
+printf("\n");
+}
