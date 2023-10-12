0dd4f1ce4e09078aa3088b461f71df68f696d512
kareemwally <kareemwally@std.mans.edu.eg>
2023-10-11 18:23:05 -0700

    please work

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include"function_pointers.h"
+/**
+ *print_name-function
+ *@name:the string we need to print
+ *@f:the pointer to the printing-function
+ *Return:(void)
+ */
+void print_name(char *name,void (*f)(char *))
+{
+f(name;)
+}
