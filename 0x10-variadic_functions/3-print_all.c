a7e845234a64a45e09ed9e8786869b618398bee2
kareemwally <kareemwally@std.mans.edu.eg>
2023-10-12 19:09:03 -0700

    anything gets printing

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+/**
+ *print_all-funcction
+ *description:this function prints whatever type
+ *@format:pointer to the data type to print
+ *Return:(void)
+ */
+void print_all(const char * const format, ...)
+{
+
+}
