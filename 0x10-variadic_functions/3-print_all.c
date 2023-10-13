a6d0affff7b3b62af92ee3c63d83141fc676d1f7
kareemwally <kareemwally@std.mans.edu.eg>
2023-10-12 19:44:15 -0700

    printing anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,5 +7,34 @@
  */
 void print_all(const char * const format, ...)
 {
-
+size_t i = 0;
+char g;
+va_list res;
+va_start(res, format);
+while (i <  strlen(format))
+{
+g = format[i];
+switch (g)
+{
+case 'i':
+printf("%d", va_arg(res, int));
+break;
+case 'f':
+printf("%f", va_arg(res, float));
+break;
+case 'c':
+printf("%c", va_arg(res, char));
+break;
+case 's':
+printf("%s", va_arg(res, char *));
+break;
+}
+if (i != strlen(format) - 1)
+{
+printf(", ");
+}
+i++;
+}
+va_end(res);
+printf("\n");
 }
