5ced040a1fe9dc77e2e14a339eb162a693fdcb8c
kareemwally <kareemwally@std.mans.edu.eg>
2023-10-19 12:24:30 -0700

    nimber of nodes

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ *list_len-function
+ *Description:this function returns number of nodes
+ *@h:pointer to 1st node
+ *Return:(int)
+ */
+size_t list_len(const list_t *h)
+{
+size_t res = 0;
+const list_t *n = NULL;
+if (h == NULL)
+{
+res = 1;
+return (res);
+}
+n = h;
+while (n != NULL)
+{
+res++;
+n = n->next;
+}
+return (res);
+}
