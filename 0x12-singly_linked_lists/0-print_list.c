25956eac60e130b4e4be6cce986f25ea4e5cd492
kareemwally <kareemwally@std.mans.edu.eg>
2023-10-19 11:10:37 -0700

    here we go

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ *print_list-function
+ *Description:this function
+ *@h:pointer of type list_t to next node
+ *Return:(int)
+ */
+size_t print_list(const list_t *h)
+{
+size_t res = 0;
+list_t *n = NULL;
+n = h;
+while (n != NULL)
+{
+res++;
+printf("[%d] %s\n", n->len, n->str ? n->str : "(nil)");
+n = n->next;
+}
+return (res);
+}
