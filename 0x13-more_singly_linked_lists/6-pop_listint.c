f308aa5ac7c89e73d29e29548212cbbaafc06c73
kareemwally <kareemwally@std.mans.edu.eg>
2023-10-24 12:44:45 -0700

    popping 1st element

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,5 +1,21 @@
 #include "lists.h"
 /**
+ *listint_len - function
+ *description:this function only tells the number of nodes
+ *@h:the pointer to the next node
+ *Return:(int)
+ */
+size_t listint_len(const listint_t *h)
+{
+size_t res = 0;
+while (h != NULL)
+{
+res++;
+h = h->next;
+}
+return (res);
+}
+/**
  *pop_listint-function
  *description:that function returns the first element
  *@head:the base address of the first node in list
