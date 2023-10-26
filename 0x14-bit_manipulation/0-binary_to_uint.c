f2d2d04892358714ae287ffeed66cfe1f71b6bee
kareemwally <kareemwally@std.mans.edu.eg>
2023-10-26 16:20:10 -0700

    from binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,28 +1,4 @@
 #include "main.h"
-#include <math.h>
-/**
- *binary_to_uint-function
- *description:that function converts binary to int
- *@b:pointer to binary word
- *Return:(int)
- */
-unsigned int binary_to_uint(const char *b)
-{
-int res = 0;
-if (b == NULL || is_only_binary(b) == 0)
-{
-return (10);
-}
-else
-{
-for (i = 0; i < strlen(b); i++)
-{
-if(b[i] == 49)
-res += (pow(2, (strlen(b) - 1 - i)));
-}
-}
-return (res);
-}
 /**
  *is_only_binary-function
  *description:that function checks the pointer
@@ -39,5 +15,25 @@ if (!(b[i] == 48 || b[i] == 49))
 return (0);
 }
 }
-return (1);
+/**
+ *binary_to_uint-function
+ *description:that function converts binary to int
+ *@b:pointer to binary word
+ *Return:(int)
+ */
+unsigned int binary_to_uint(const char *b)
+{
+int res = 0;
+if (b == NULL || is_only_binary(b) == 0)
+{
+return (0);
+}
+else
+{
+while (*b)
+{
+res = res * 2 + (*b++ - '0');
+}
+}
+return (res);
 }
