57927055376d5a349b66718a884fb2689f0c83c5
kareemwally <kareemwally@std.mans.edu.eg>
2023-12-23 06:36:57 -0800

    printing nodes

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+#include <string.h>
+/**
+ * hash_table_print - functio to print H_table
+ * Description: that function to print nodes in hash_table
+ * @ht: the hash_table to be print
+ * Return: (void)
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+hash_node_t *res;
+unsigned long int i;
+printf("{");
+if (ht != NULL || ht->array != NULL)
+{
+for (i = 0; i < ht->size; i++)
+{
+res = ht->array[i];
+while (res != NULL)
+{
+printf("\'%s\': \'%s\',", res->key, res->value);
+res = res->next;
+}
+}
+}
+printf("}\n");
+}
