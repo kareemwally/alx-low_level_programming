db0f79826da3578c53dff31e8ee0b2cf897e27eb
kareemwally <kareemwally@std.mans.edu.eg>
2023-12-24 09:26:31 -0800

    deleting all nodes

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,5 +7,24 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-
+unsigned long int index;
+hash_node_t *tmp;
+if (ht == NULL || ht->array == NULL || ht->size == 0)
+{
+return;
+}
+for (index = 0; index < ht->size; index++)
+{
+while (ht->array[index] != NULL)
+{
+tmp = ht->array[index]->next;
+free(ht->array[index]->key);
+free(ht->array[index]->value);
+free(ht->array[index]);
+ht->array[index] = tmp;
+}
+}
+free(ht->array);
+free((void *)ht->size);
+free(ht);
 }
