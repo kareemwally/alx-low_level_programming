511257c0fea51398956e5cfe6800f7a057559ad6
kareemwally <kareemwally@std.mans.edu.eg>
2023-12-24 09:05:51 -0800

    deleting hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - function to delete the ht
+ * Description: that function frees the data and keys
+ * @ht: the pointer to the hash table
+ * Return: (void)
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+
+}
