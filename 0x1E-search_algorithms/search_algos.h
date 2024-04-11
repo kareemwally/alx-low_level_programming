620a8a571ceaccacbbbd7d22b37f9fb7e381d572
kareem wally <kareemwally@std.mans.edu.eg>
2024-04-11 10:54:09 +0200

    header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef SEARCH_ALGOS_H_INCLUDED
+#define SEARCH_ALGOS_H_INCLUDED
+#include <stdlib.h>
+#include <stdio.h>
+int linear_search(int *array, size_t size, int value);
+
+
+#endif // SEARCH_ALGOS_H_INCLUDED
