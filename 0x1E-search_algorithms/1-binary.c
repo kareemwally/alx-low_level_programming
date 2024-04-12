18d06bc869732888f22c351238f5073b0e78791b
kareem wally <kareemwally@std.mans.edu.eg>
2024-04-11 19:39:09 -0700

    BS

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,37 +1,38 @@
 #include "search_algos.h"
 
 int binary_search(int *array, size_t size, int value) {
-if (array == NULL || size == 0) {
- return -1;
-}
-int left = 0;
-int right = size - 1;
-int mid;
-while (left <= right) {
-mid = left + (right - left) / 2;
-printf("Searching in array: ");
-for (size_t i = left; i <= right; i++)
-{
-printf("%d", array[i]);
-if (i != right)
-{
-printf(", ");
-}
-}
-printf("\n");
-if (array[mid] == value)
-{
-return mid;
-}
-else if (array[mid] < value)
-{
-left = mid + 1;
+	int left = 0;
+	int right = size - 1;
+	int mid;
+	int i = 0;
+	if (array == NULL || size == 0) {
+		return -1;
+	}
+	while (left <= right) {
+	mid = left + (right - left) / 2;
+	printf("Searching in array: ");
+	for (i = left; i <= right; i++)
+	{
+		printf("%d", array[i]);
+		if (i != right)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	if (array[mid] == value)
+	{
+		return (mid);
+	}
+	else if (array[mid] < value)
+	{
+		left = mid + 1;
         
-}
-else
-{
-right = mid - 1;
-}
-}
-return -1;
+	}
+	else
+	{
+		right = mid - 1;
+	}
+	}
+	return (-1);
 }
