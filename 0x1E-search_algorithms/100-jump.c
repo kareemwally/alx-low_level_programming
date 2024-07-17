e8c48530b45c98b77f60dd8406737bb2a6659131
kareem <kareemwally@std.mans.edu.eg>
2024-07-17 21:16:42 +0300

    interval of sqrt n

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+/**
+ *jump_search - function
+ *description: this function implements jump search
+ *@value: number to search for
+ *@array: pointer to 1st element
+ *@size: total number of array
+ *Return: (int)
+ */
+int jump_search(int *array, size_t size, int value)
+{
+
+}
