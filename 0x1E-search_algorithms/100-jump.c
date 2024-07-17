958b5e19a403c0d738fec1cce9deb7f6b4650f0f
kareem <kareemwally@std.mans.edu.eg>
2024-07-17 21:47:23 +0300

    inderval of sqrt(n)

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,4 +1,5 @@
 #include "search_algos.h"
+#include <math.h>
 /**
  *jump_search - function
  *description: this function implements jump search
@@ -9,5 +10,24 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
+	int start = 0;
+	size_t end = sqrt(size);
 
+	while (array[end - 1] < value)
+	{
+		if (size - 1 == end && array[end - 1] != value)
+			return (-1);
+		start = end;
+		end = end + sqrt(size);
+	}
+	if (array[end - 1] == value)
+		return (end - 1);
+
+	while (array[end - 1] > value)
+	{
+		if (array[start] == value)
+			break;
+		start++;
+	}
+	return (start);
 }
