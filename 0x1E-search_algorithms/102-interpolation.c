76a938d942617baf29f1b64e75fb36092bb8dbd9
kareem <kareemwally@std.mans.edu.eg>
2024-07-18 04:06:48 +0300

    interpolation

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,59 @@
+#include "search_algos.h"
+/**
+ *get_pos - function
+ *description: finding the position
+ *@value: number to be found
+ *@array: list of numbers to search in
+ *@lo: array's lower boundary number
+ *@hi: array's higher boundary number
+ *Return: (int)
+ */
+size_t get_pos(size_t lo, size_t hi, int *array, int value)
+{
+return (lo + (((double)(hi - lo) / (array[hi] - array[lo])) * (value - array[lo])));
+}
+/**
+ *interpolation_search - function
+ *description: this function implements interpolation algorithm
+ *@value: number to be found
+ *@array: list of numbers to search in
+ *@size: array's number of elements
+ *Return: (int)
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+
+	size_t lo = 0, hi = size - 1, mid = -1;
+	int index = -1;
+
+	if (!array)
+		return (-1);
+	while (lo <= hi)
+	{
+		mid = get_pos(lo, hi, array, value);
+		if (mid >= size)
+		{
+			printf("Value checked array[%ld] is out of range\n", mid);
+			return (-1);
+		}
+		printf("Value checked array[%ld] = [%d]\n", mid, array[mid]);
+		if (array[mid] == value)
+		{
+			index = mid;
+			break;
+		}
+		else
+		{
+			if (array[mid] < value)
+			{
+				lo = mid + 1;
+			}
+			else
+			{
+				hi = mid - 1;
+			}
+		}
+	}
+	printf("Found %d at index: %d\n", value, index);
+	return (index);
+}
